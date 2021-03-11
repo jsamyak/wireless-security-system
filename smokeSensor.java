d66144f6b35561777ebe8fe0e074d6e6934cc3f4
Samyak Jain <iamsamyakjain@gmail.com>
2021-03-12 01:39:29 +0530

    Minor fix

--- smokeSensor.java
+++ smokeSensor.java
@@ -1,53 +1,46 @@
-/**
- * TCSS 360 - Project 3
- * 
- * @author hphobbs
- * @version Winter 2021
- *
- */
-
 public class SmokeSensor extends SecurityDevice {
 
-	private static int mySmokeLevel;
-	private static final int mySmokeThreshold = 50;
-	private static boolean myAlert = false;
-	private static boolean isArmed = true;
+    private static int mySmokeLevel;
+    private static final int mySmokeThreshold = 50;
+    private static boolean myAlert;
+    private static boolean isArmed;
 
-	public SmokeSensor() {
-		super(deviceID);
-		mySmokeLevel = 0;
-	}
+    public SmokeSensor() {
+        super(deviceID);
+        mySmokeLevel = 0;
+        myAlert = false;
+        isArmed = true;
+    }
 
-	public int getSmokeLevel() {
-		return mySmokeLevel;
-	}
+    public int getSmokeLevel() {
+        return mySmokeLevel;
+    }
 
-	private void armDevice(boolean isArmed) {
-		if (isArmed) {
-			isArmed = true;
-			System.out.println("Smoke Sensor armed.");
-		} else {
-			isArmed = false;
-			System.out.println("Smoke Sensor disarmed.");
-		}
-	}
+    private void armDevice(boolean isArmed) {
+        if (isArmed) {
+            isArmed = true;
+            System.out.println("Smoke Sensor armed.");
+        } else {
+            isArmed = false;
+            System.out.println("Smoke Sensor disarmed.");
+        }
+    }
 
-	public static void sendAlert() {
-		if (isArmed && (mySmokeLevel >= mySmokeThreshold)) {
-			myAlert = true;
-			System.out.println("ALERT! SMOKE  DETECTED!");
-		} else {
-			myAlert = false;
-			System.out.println("Not Smoke Detected");
-		}
+    public void sendAlert() {
+        if (isArmed && (mySmokeLevel >= mySmokeThreshold)) {
+            myAlert = true;
+            super.sendAlert("ALERT! SMOKE  DETECTED!");
+        } else {
+            myAlert = false;
+            super.sendAlert("No Smoke Detected");
+        }
+    }
 
-	}
+    /*
+     * Stop Alerting a Fire
+     */
+    public void reset() {
 
-	/*
-	 * Stop Alerting a Fire
-	 */
-	public void reset() {
-		myAlert = false;
-		isArmed = true;
-	}
+    }
+
 }
